f946a68e85ce20665be34320f22e6360b5603839
AriadneT <AriadneT@users.noreply.github.com>
2017-12-20 23:04:34 +0100

    Templating through PHP begun
    Templating through PHP begun. Two new icons. More detailed readme file.  Music added. Background picture renamed. Readability of JavaScript and PHP files improved.

--- Functions/Main/CalendarDisplayer.php
+++ Functions/Main/CalendarDisplayer.php
@@ -4,7 +4,37 @@
  */
 function showPage()
 {
-	$HTML = file_get_contents('../Adventskalender/Resources/Private/Templates/Main.html');
+	$replacements = [];
+	$today = getdate();
+	
+	if ($today['month'] != 'December') {
+		$replacements['#message#'] = 
+			'Sorry, this advent calendar is only available in December.';
+	} else {
+		$replacements['#message#'] = 
+			'Sorry, this advent calendar is not yet ready? It will be available in 2018.';
+	}
+	$HTML = 
+		file_get_contents('../Adventskalender/Resources/Private/Templates/Main.html');
+		
+	// Find all words to be replaced. These have '#' before and after the word(s).
+    $splitHTMLContent = preg_split('/[<>]/', $HTML);
+    $toReplace = [];
+    foreach ($splitHTMLContent as $subset) {
+        if (preg_match('/#[a-z]+#/', $subset)) {
+            $toReplace[] = $subset;
+        }
+    }
+	
+	// Replace words to be replaced with labels and results from $replacements
+    foreach ($replacements as $key => $value) {
+        foreach ($toReplace as $entry) {
+            if (preg_match('/' . $entry . '/', $key)) {
+                $HTML = str_replace($entry, $value, $HTML);
+                break;
+            }
+        }
+    }
 	
 	return $HTML;
 }
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Adventskalender
+
+Erstellt: 16.12.2017 (erste Version)
+
+Version: 1.0
+
+Zweck: Adventskalender für 2018 für Verwandten und Freundinnen vorzubereiten.
+
+**Voraussetzungen**
+
+Keine.
+
+**Ausfuehrungen zum Betrieb**
+
+Die Webseite soll im Dezember (ab 2018) einen Adventskalender zeigen. Welche 
+Bilder gezeigt werden hängt davon ab, welcher Tag die Website abgerufen wird.
