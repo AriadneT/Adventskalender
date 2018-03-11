331253b4e4d960b2402cf080582ceeecc1626bcf
AriadneT <AriadneT@users.noreply.github.com>
2018-03-11 21:09:03 +0100

    All windows show appropriate icon
    All windows now show an appropriate icon, also when they are open. JavaScript functions better integrated with each other.

--- Resources/Public/JavaScript/Displayer.js
+++ Resources/Public/JavaScript/Displayer.js
@@ -1,9 +1,37 @@
+var images = [
+	'Bauble',
+	'Dove',
+	'Tree',
+	'Bow',
+	'Bells',
+	'Present',
+	'Candles',
+	'CandyCane',
+	'Stocking',
+	'Wreath',
+	'Angel',
+	'Snowman',
+	'Santa',
+	'GingerbreadMan',
+	'Rudolph',
+	'Teddy',
+	'Snowflake',
+	'SmallStar',
+	'Heart',
+	'Present2',
+	'Bauble2',
+	'Bell',
+	'Girl',
+	'Marbles',
+	'Star',
+]
+var imagePath = '../../../../Adventskalender/Resources/Public/Images/'
+
 function arrangeCalendar() {
 	reduceVolume();
 	
 	var today = new Date();
 	checkAvailability(today);
-	setIcons(today);
 }
 
 function reduceVolume() {
@@ -16,9 +44,14 @@ function checkAvailability(today) {
     if (month < 11) {
         document.getElementsByTagName('h2')[0].innerHTML = 
 			'Sorry, this advent calendar is only available in December.';
+		var dayInMonth = today.getDate();
+		for (day = 1; day < 26; day++) {
+			var window = document.getElementById('day' + day);
+			window.src = imagePath + 'Window' + day + '.png';
+		}
     } else {
-		var day = today.getDate();
 		document.getElementsByTagName('h2')[0].innerHTML = '';
+		setIcons(today);
 	}
 }
 
@@ -27,12 +60,10 @@ function setIcons(today) {
 	for (day = 1; day < 26; day++) {
 		var window = document.getElementById('day' + day);
 		if (day <= dayInMonth) {
-			window.src = 
-				'../../../../Adventskalender/Resources/Public/Images/Bauble.png';
+			var image = images[day - 1];
+			window.src = imagePath + image + '.png';
 		} else {
-			window.src = 
-				'../../../../Adventskalender/Resources/Public/Images/Window' + 
-				day + '.png';
+			window.src = imagePath + 'Window' + day + '.png';
 		}
 	}
 }
